a17152a2503f9456110ba2c5f105d6d0df538823
Govinda Paliwal <govindapaliwal13@gmail.com>
2015-04-28 20:27:32 +0530

    initial commit

---
+++ gen/com/govinda/gplightwidget/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.govinda.gplightwidget;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/govinda/gplightwidget/FlashlightWidgetProvider.java
@@ -0,0 +1,29 @@
+package com.govinda.gplightwidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import com.govinda.gplightwidget.R;
+
+public class FlashlightWidgetProvider extends AppWidgetProvider {
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		//		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		Intent receiver = new Intent(context, FlashlightWidgetReceiver.class);
+        receiver.setAction("COM_FLASHLIGHT");
+        receiver.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, receiver, 0);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+        views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetIds, views);
+        
+	}
+
+}
